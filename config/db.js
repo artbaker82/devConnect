c0a97ff916fc35c6c0a8555102c948fa4ee38332
Arthur Baker <artbaker82@gmail.com>
2021-10-05 20:39:02 -0700

    post users api endpoint

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-//importing mongose URI from monfig json file
+//importing mongose URI from config json file
 const config = require("config");
 const db = config.get("mongoURI");
 
--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const connectDB = require('./config/db')
 
-//looks for port env file (for deployment)
 
 const app = express();
 //Connect to database
 connectDB();
+
+//init middleware
+app.use(express.json({extended: false}));
 
 app.get('/', (req,res) => res.send('API running'))
 
@@ -15,6 +17,7 @@ app.use('/api/auth', require('./routes/api/auth'))
 app.use('/api/profile', require('./routes/api/profile'))
 app.use('/api/posts', require('./routes/api/posts'))
 
+//looks for port env file (for deployment)
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`))
