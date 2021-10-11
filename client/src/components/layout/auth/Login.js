1159d3d1b68f9a171f415b077300d3679d2dc7b2
Arthur Baker <artbaker82@gmail.com>
2021-10-10 18:21:56 -0700

    started react frontend, login and register page

---
+++ client/src/components/layout/auth/Login.js
@@ -0,0 +1,56 @@
+import React, { Fragment, useState } from "react";
+import { Link } from "react-router-dom";
+
+export const Login = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  //desctructuring variables from formData state
+  const { email, password } = formData;
+  //function to change form data, using computed property names
+  const onChange = (e) => setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    console.log("success");
+  };
+
+  return (
+    <Fragment>
+      <h1 className='large text-primary'>Sign In</h1>
+      <p className='lead'>
+        <i className='fas fa-user'></i> Sign into your account
+      </p>
+      <form className='form' onSubmit={(e) => onSubmit(e)}>
+        <div className='form-group'>
+          <input
+            type='email'
+            placeholder='Email Address'
+            name='email'
+            value={email}
+            onChange={(e) => onChange(e)}
+            required
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            type='password'
+            placeholder='Password'
+            name='password'
+            value={password}
+            onChange={(e) => onChange(e)}
+            minLength='6'
+            required
+          />
+        </div>
+
+        <input type='submit' className='btn btn-primary' value='Login' />
+      </form>
+      <p className='my-1'>
+        Dont have an account? <Link to='/register'>Sign Up</Link>
+      </p>
+    </Fragment>
+  );
+};
