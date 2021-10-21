d094c76cc99a54906fd61ee000807d1775c63a7f
Arthur Baker <artbaker82@gmail.com>
2021-10-21 15:52:03 -0700

    profile and profileItems

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import { loadUser } from "./actions/auth";
 import EditProfile from "./components/profile-forms/EditProfile";
 import AddExperience from "./components/profile-forms/AddExperience";
 import AddEducation from "./components/profile-forms/AddEducation";
+import Profiles from "./components/profiles/Profiles";
 //redux
 import { Provider } from "react-redux";
 import store from "./store";
@@ -41,6 +42,7 @@ const App = () => {
             <Switch>
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
+              <Route exact path='/profiles' component={Profiles} />
               {/* PrivateRoute component to check authentication for dashboard */}
               <PrivateRoute exact path='/dashboard' component={Dashboard} />
               <PrivateRoute exact path='/create-profile' component={CreateProfile} />
