9d1e59f6f13b04c1c11f0ee0947daaf546887eeb
Arthur Baker <artbaker82@gmail.com>
2021-10-25 13:27:25 -0700

    posts

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import alert from "./alert";
 import auth from "./auth";
 import profile from "./profile";
+import post from "./post";
 //this is the rootReducer
 export default combineReducers({
   alert,
   auth,
   profile,
+  post,
 });
