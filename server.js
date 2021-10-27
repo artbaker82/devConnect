8c0ec3e50013c5d7246be612cf124ba3559a52e5
Arthur Baker <artbaker82@gmail.com>
2021-10-26 19:52:21 -0700

    ready for deployment

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/db");
+//core nodejs module to manipulate file paths
+const path = require("path");
 
 const app = express();
 //Connect to database
@@ -8,16 +10,24 @@ connectDB();
 //init middleware
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => res.send("API running"));
-
 //define routes
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
 
+//SERVE STATIC ASSETS IN PRODUCTION
+
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 //looks for port env file (for deployment)
-//const PORT = process.env.PORT || 5000;
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
